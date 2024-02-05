30669879ac2a79838ebcdc3deb0f2eca0508fce4
EdmondDantes21 <redi.vreto@studenti.unitn.it>
2024-02-05 18:02:16 +0100

    added sequential version of the main algorithm

--- src/divide_and_conquer/closest_pair.cpp
+++ src/divide_and_conquer/closest_pair.cpp
@@ -1,5 +1,4 @@
 #include <bits/stdc++.h>
-#include <mpi.h>
 using namespace std;
 
 class Point {
@@ -8,134 +7,83 @@ public:
     double y;
 };
 
-vector<Point> generate_points(int); 
-vector<Point> merge_two_vectors(vector<Point> &A, vector<Point> &B, bool sort_by_x);
-vector<Point> parallel_merge_sort(vector<Point> &A, int world_rank, int world_size, bool sort_by_x);
-void merge_sort(vector<Point> &A, int start, int end, bool sort_by_x);
-void merge(vector<Point> &A, int start, int end, int mid, bool sort_by_x);
-
 int N;
 
-int main(int argc, char **argv) {
-    if (argc == 0)
+vector<Point> generate_points(int); 
+double brute_force(vector<Point>& points);
+inline double dist(Point& p1, Point& p2);
+double min_dist(vector<Point>& x_sorted, vector<Point>& y_sorted);
+
+int main(int argc, char** argv) {
+    if (argc == 1)
         return 0;
     N = atoi(argv[1]);
 
-    MPI_Init(NULL, NULL);
-    int world_size, world_rank;
-    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
-    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
-<
-    vector<Point> points;
-    if (world_rank == 0)
-        points = generate_points(N);
-  
-    vector<Point> points_x = parallel_merge_sort(points, world_rank, world_size, true); // points sorted by x for process 0
-    points.clear();
+    vector<Point> x_sorted = generate_points(N);
+    vector<Point> y_sorted = x_sorted;
+    
+    sort(x_sorted.begin(), x_sorted.end(), [](Point const & a, Point const & b) -> bool
+        { return a.x < b.x; } 
+    );
+    sort(y_sorted.begin(), y_sorted.end(), [](Point const & a, Point const & b) -> bool
+        { return a.y < b.y; } 
+    );
 
-    
-    
-    MPI_Finalize();
-    return 0;
+    cout << "MIN DIST = " << min_dist(x_sorted, y_sorted) << endl;
 }
 
-vector<Point> parallel_merge_sort(vector<Point> &points, int world_rank, int world_size, bool sort_by_x) {
-    // custom data type
-    MPI_Datatype dt_point;
-    MPI_Type_contiguous(2, MPI_DOUBLE, &dt_point);
-    MPI_Type_commit(&dt_point);
-
-    vector<Point> local_points(N / world_size); // local buffer to store a piece of the vector
-    MPI_Scatter(points.data(), N / world_size, dt_point, local_points.data(), N / world_size, dt_point, 0, MPI_COMM_WORLD);
+double min_dist(vector<Point>& x_sorted, vector<Point>& y_sorted) {
+    int n = x_sorted.size();
+    if (n < 10) 
+        return brute_force(x_sorted);
     
-    merge_sort(local_points, 0, (N / world_size) - 1, sort_by_x);
+    Point median = x_sorted[n / 2];
+    vector<Point> x_sorted_left = vector<Point>(x_sorted.begin(), x_sorted.begin() + n / 2);
+    vector<Point> x_sorted_right = vector<Point>(x_sorted.begin() + n / 2 + 1, x_sorted.end());
+    vector<Point> y_sorted_left;
+    vector<Point> y_sorted_right;
+
+    // assign points left of the line to ysorted_left and the others to y_sorted_right
+    for (auto &p : y_sorted)
+        if (p.x < median.x)
+            y_sorted_left.push_back(p);
+        else
+            y_sorted_right.push_back(p);
+
+    // recursively solve left and right     
+    double delta_1 = min_dist(x_sorted_left, y_sorted_left); 
+    double delta_2 = min_dist(x_sorted_right, y_sorted_right); 
+    double delta = min(delta_1, delta_2);
     
-    int size = world_size, rank = world_rank, msg_size = N / world_size;
-    while (size != 1) {
-        if (rank < size) {
-            if (rank >= size / 2) {
-                MPI_Send(local_points.data(), msg_size, dt_point, rank - size / 2, 0, MPI_COMM_WORLD);
-            } else {
-                vector<Point> tmp(msg_size);
-                MPI_Recv(tmp.data(), msg_size, dt_point, rank + size / 2, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-                local_points = merge_two_vectors(local_points, tmp, sort_by_x);
-            }
-        }
-        size /= 2;
-        msg_size *= 2;
-    }
-
-    if (world_rank != 0)
-        local_points.clear();
-
-    return local_points;
+    // put points whose distance is at most delta from the vertical strip inside strip
+    vector<Point> strip;
+    for (auto &p : y_sorted) 
+        if (p.x < median.x + delta && p.x > median.x - delta)
+            strip.push_back(p);
+    
+    // for all points in strip check the next 6 points down the strip
+    for (int i = 0; i < strip.size(); i++) 
+        for (int j = i + 1; j < i + 7 && j < strip.size(); j++) 
+            delta = min(delta, dist(strip[i], strip[j]));
+        
+    return delta;
 }
 
-vector<Point> merge_two_vectors(vector<Point> &A, vector<Point> &B, bool sort_by_x) {
-    int n = A.size();
-    vector<Point> C(n * 2);
-    int i = 0, j = 0, k = 0;
+double brute_force(vector<Point>& points) {
+    double min_dist = dist(points[0], points[1]);
+    int n = points.size();
 
-    while (i < n && j < n) {
-        if (sort_by_x) {
-            if (A[i].x <= B[j].x)
-                C[k++] = A[i++];
-            else    
-                C[k++] = B[j++];
-        } else {
-            if (A[i].y <= B[j].y)
-                C[k++] = A[i++];
-            else    
-                C[k++] = B[j++];
-        }
-    }
-
-    if (i == n)
-        while (j < n)
-            C[k++] = B[j++];
-    else 
-        while (i < n)
-            C[k++] = A[i++];
-
-    return C;
+    for (int i = 0; i < n; i++) 
+        for (int j = i + 1; j < n; j++) 
+            min_dist = min(min_dist, dist(points[i], points[j]));
+        
+    return sqrt(min_dist); 
 }
 
-void merge_sort(vector<Point> &A, int start, int end, bool sort_by_x) {
-    if (start < end) {
-        int mid = floor((start + end) / 2);
-        merge_sort(A, start, mid, sort_by_x);
-        merge_sort(A, mid + 1, end, sort_by_x);
-        merge(A, start, end, mid, sort_by_x);
-    }
-}
-
-void merge(vector<Point> &A, int start, int end, int mid, bool sort_by_x) {
-    int i = start, j = mid + 1, k = 0;
-    vector<Point> B(end - start + 1);
- 
-    while (i <= mid && j <= end) {
-        if (sort_by_x) {
-            if (A[i].x <= A[j].x) 
-                B[k++] = A[i++];
-            else
-                B[k++] = A[j++];
-        } else {
-            if (A[i].y <= A[j].y) 
-                B[k++] = A[i++];
-            else
-                B[k++] = A[j++];
-        }
-    }
-
-    if (i > mid) 
-        while (j <= end)
-            B[k++] = A[j++];
-    else 
-        while (i <= mid)
-            B[k++] = A[i++];
-
-    for (int l = 0; l < k; l++)
-        A[start++] = B[l];
+inline double dist(Point& p1, Point& p2) {
+    double delta_x = fabs(p1.x - p2.x);
+    double delta_y = fabs(p1.y - p2.y);
+    return delta_x * delta_x + delta_y * delta_y;
 }
 
 vector<Point> generate_points(int n) {
